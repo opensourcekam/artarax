3bede93543175ba7b9474bf390869206bd22899e
opensourcekam <kamdroidR@me.com>
2019-06-16 21:32:17 -0400

    update

--- src/utils/getCurrentUrl.ts
+++ src/utils/getCurrentUrl.ts
@@ -1,4 +1,4 @@
 import { Request, Response } from 'express';
-const getCurrentUrl = (req: Request, _: Response): string => `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+const getCurrentUrl = (req: Request, _: Response): string => req.url;
 
 export { getCurrentUrl };
